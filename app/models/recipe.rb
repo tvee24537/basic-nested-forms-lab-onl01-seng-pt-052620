1773ea98139363fda843d178e7b3bb2a14c78fc2
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-17 18:39:03 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
+
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+    ingredients.each do |ingr|
+      self.ingredients << Ingredient.find_or_create_by(ingr[1])
+      self.save
+    end
+  end
 end
