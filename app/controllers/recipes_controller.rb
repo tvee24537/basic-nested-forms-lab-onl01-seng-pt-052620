84be45b36ddbefc138077de253da3f1347769ecf
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-17 18:42:26 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :recipe_params, only: [:create]
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +11,22 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build(recipe_id: @recipe.id) }
+
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
