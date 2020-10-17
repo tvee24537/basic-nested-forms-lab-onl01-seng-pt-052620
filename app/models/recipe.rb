d7d0725ca09229f2e0b91a8edc012a23dc8fb194
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-17 18:41:47 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,4 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
-  accepts_nested_attributes_for :ingredients
-
-  def ingredients_attributes=(ingredients)
-    ingredients.each do |ingr|
-      self.ingredients << Ingredient.find_or_create_by(ingr[1])
-      self.save
-    end
-  end
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
